f27dff71c1b629905ae5ea3588883ecf522147ae
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-30 20:18:03 +0200

    fix index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,18 @@
 const express = require('express')
 const app = express()
-const cors = 
+const cors = require('./middlewares/cors')
+const { mongoose } = require('mongoose');
+
+const connectionString = 'mongodb://127.0.0.1:27017/motorcycle';
+const port = 'http://localhost:3030'
+
+const initDB = mongoose.connect(connectionString)
+
+startServer()
+async function startServer() {
+    initDB();
+    app.use(express.json())
+    app.use(cors())
+    //trimBody
+     
+}
