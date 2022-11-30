b9969630c8ba1e1e967ef0329eb1a42fecb7b282
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-30 20:00:48 +0200

    add user model

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -2,6 +2,5 @@ module.exports = () => (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS,GET,POST,PUT,DELET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
-
     next()
 }
