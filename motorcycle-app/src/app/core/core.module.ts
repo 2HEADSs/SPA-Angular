c492ec58553dbb460eab91052990e0b5257deb52
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-11-30 15:07:38 +0200

    add core module with header, main and footer components

---
+++ motorcycle-app/src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
