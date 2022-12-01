b0c14e75fe29e0f1eef7e85e9df4e5b16206d649
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-01 15:10:02 +0200

    add register and login functionality

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,12 @@
+const { authController } = require('./controllers/authController');
+
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
+    console.log(req);
     res.json({ message: 'Rest Service Operational' })
 });
+
+router.use('/user', authController)
 
 module.exports = router
