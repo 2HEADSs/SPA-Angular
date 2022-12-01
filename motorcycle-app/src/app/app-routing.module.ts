371f6b5c9880835e228a650a55e36b1054491eae
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-01 23:25:20 +0200

    add path to login and register

--- motorcycle-app/src/app/app-routing.module.ts
+++ motorcycle-app/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { MainComponent } from './core/main/main.component';
 
 const routes: Routes = [
@@ -7,6 +9,14 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: MainComponent
+  },
+  {
+    path: 'auth/register',
+    component: RegisterComponent
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent
   },
 ];
 
--- motorcycle-app/src/app/app.module.ts
+++ motorcycle-app/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
@@ -12,7 +14,9 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    AuthModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
