fb1c0e490808a10eb90651c04618ed581cbe1c0b
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-01 16:38:54 +0200

    add login component

--- motorcycle-app/src/app/auth/auth.module.ts
+++ motorcycle-app/src/app/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
