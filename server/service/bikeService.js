319ea65685b72b75457b49873d314a54ce8e5b8f
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-05 09:51:38 +0200

    update bikeService

---
+++ server/service/bikeService.js
@@ -0,0 +1,45 @@
+const Bike = require('../models/Bike')
+
+
+async function getAll() {
+    return Bike.find({})
+
+};
+async function getByUserId(userId) {
+    return Bike.find({ _ownerId: userId })
+
+};
+async function getById(id) {
+    return Bike.findById(id)
+};
+
+async function create(item) {
+    return Bike.create(item)
+};
+
+async function update(id, bike) {
+    const existing = await Bike.findById(id);
+
+    existing.brand = bike.brand;
+    existing.model = bike.model;
+    existing.year = bike.year;
+    existing.power = bike.power;
+    existing.price = bike.price;
+    existing.description = bike.description;
+    existing.img = bike.img;
+    return existing.save()
+}
+
+async function deleteById(id) {
+    return Bike.findByIdAndDelete(id)
+};
+
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    deleteById,
+    getByUserId
+}
