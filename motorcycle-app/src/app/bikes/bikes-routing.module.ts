71452909ceda5f5f7f07610bb785b5c1d2cbd631
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-05 20:55:04 +0200

    update routes

--- motorcycle-app/src/app/bikes/bikes-routing.module.ts
+++ motorcycle-app/src/app/bikes/bikes-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BikeDetailsComponent } from './bike-details/bike-details.component';
 import { BikesListComponent } from './bikes-list/bikes-list.component';
 import { CreateBikesComponent } from './create-bikes/create-bikes.component';
 
@@ -11,6 +12,9 @@ const routes: Routes = [
 {
     path: 'bikes/create',
     component: CreateBikesComponent
+},{
+  path: 'bikes/details',
+  component: BikeDetailsComponent
 },
 ];
 
