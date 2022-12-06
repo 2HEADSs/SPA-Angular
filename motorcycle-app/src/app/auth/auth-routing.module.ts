7b69b764491ca76c883937af58e11111d3bebb7f
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-06 22:35:39 +0200

    add logout route

--- motorcycle-app/src/app/auth/auth-routing.module.ts
+++ motorcycle-app/src/app/auth/auth-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -16,6 +17,10 @@ const routes: Routes = [
     {
         path: 'auth/profile',
         component: ProfileComponent
+    },
+    {
+        path: 'auth/logout',
+        component: LogoutComponent
     },
 ];
 
