b6687a78d0fdee8283865f601f5bc0a1ba455043
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-07 20:09:35 +0200

    remove email validators

--- motorcycle-app/src/app/shared/validators/app-email.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppEmailDirective } from './app-email.directive';
-
-describe('AppEmailDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AppEmailDirective();
-    expect(directive).toBeTruthy();
-  });
-});
