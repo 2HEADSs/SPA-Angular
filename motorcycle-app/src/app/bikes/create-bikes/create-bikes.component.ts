bacd18cb0c3390fd75fc5bce7422397649f72a69
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-09 00:23:37 +0200

    update create bike html

--- motorcycle-app/src/app/bikes/create-bikes/create-bikes.component.ts
+++ motorcycle-app/src/app/bikes/create-bikes/create-bikes.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-create-bikes',
   templateUrl: './create-bikes.component.html',
   styleUrls: ['./create-bikes.component.css']
 })
-export class CreateBikesComponent implements OnInit {
-
-   URL_PATTERN = '/https?:\/\/./i'
+export class CreateBikesComponent {
 
 
-  constructor() { }
+  URL_PATTERN = '/https?:\/\/./i'
 
-  ngOnInit(): void {
+  createFormGroup: FormGroup = this.formBuilder.group({
+    'brand': new FormControl('', [Validators.required, Validators.minLength(1)]),
+    'model': new FormControl('', [Validators.required, Validators.minLength(1)]),
+    'year': new FormControl('', [Validators.required, Validators.max(2022)]),
+    'power': new FormControl('', [Validators.required, Validators.min(10)]),
+    'price': new FormControl('', [Validators.required, Validators.min(0.01)]),
+    'description': new FormControl('', [Validators.required, Validators.minLength(5)]),
+    'img': new FormControl('', [Validators.required, Validators.pattern(this.URL_PATTERN)]),
+
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  createHandler(): void {
+    const { brand, model, year, power, price, description, img } = this.createFormGroup.value
+    console.log(this.createFormGroup.value);
+
   }
 
 }
