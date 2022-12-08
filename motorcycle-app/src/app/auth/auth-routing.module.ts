5abdeafb7dafb7ba6916710988754896a88c4bdb
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-08 14:47:38 +0200

    clear imports

--- motorcycle-app/src/app/auth/auth-routing.module.ts
+++ motorcycle-app/src/app/auth/auth-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from '../core/not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ProfileComponent } from './profile/profile.component';
