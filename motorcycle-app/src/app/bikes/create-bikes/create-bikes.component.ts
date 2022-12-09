7ccb9fb2c0e4fa23a0079aed9378975332e83150
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-09 14:18:10 +0200

    update create form

--- motorcycle-app/src/app/bikes/create-bikes/create-bikes.component.ts
+++ motorcycle-app/src/app/bikes/create-bikes/create-bikes.component.ts
@@ -10,13 +10,13 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class CreateBikesComponent {
 
 
-  URL_PATTERN = '/https?:\/\/./i'
+  URL_PATTERN = /^https?:\/\/.+/i
 
   createFormGroup: FormGroup = this.formBuilder.group({
     'brand': new FormControl('', [Validators.required, Validators.minLength(1)]),
     'model': new FormControl('', [Validators.required, Validators.minLength(1)]),
     'year': new FormControl('', [Validators.required, Validators.max(2022), Validators.min(1885)]),
-    'power': new FormControl('', [Validators.required, Validators.min(10)]),
+    'power': new FormControl('', [Validators.required, Validators.min(0.5)]),
     'price': new FormControl('', [Validators.required, Validators.min(0.01)]),
     'description': new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(100)]),
     'img': new FormControl('', [Validators.required, Validators.pattern(this.URL_PATTERN)]),
