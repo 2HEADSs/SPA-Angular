74698610c9af4601718ddceb17247bb57e5807e5
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-11 22:33:11 +0200

    add gard for logged in users

---
+++ motorcycle-app/src/app/shared/routeGards/hasUser.gaurd.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { AuthService } from "src/app/auth/auth.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class GuestActivate implements CanActivate {
+
+    constructor(private authService: AuthService, private router: Router) {}
+    
+    canActivate() {        
+        if(!this.authService.user) {
+            this.router.navigate(['/']);
+            return false;
+        }
+        return true;
+    }
+}
