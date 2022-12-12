8d5ec64a3f13da8f5f435d59cf22d6d4ee45344c
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-12 19:42:17 +0200

    update details page

--- motorcycle-app/src/app/bikes/bike-details/bike-details.component.ts
+++ motorcycle-app/src/app/bikes/bike-details/bike-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { IBike } from 'src/app/shared/interfaces/bikes';
 import { BikesService } from '../bikes.service';
 
 @Component({
@@ -9,17 +11,24 @@ import { BikesService } from '../bikes.service';
 })
 export class BikeDetailsComponent implements OnInit {
 
-  singleBike: any | null = null
-  
+  singleBike: IBike | null = null
+  isOwner: Boolean = false
 
-  constructor(private bikesSerice: BikesService, private activatedRoute: ActivatedRoute) { }
+  constructor(private bikesSerice: BikesService, private activatedRoute: ActivatedRoute, private currentUser: AuthService) { }
   ngOnInit(): void {
     let id = this.activatedRoute.snapshot.params['id']
-    
+
     this.bikesSerice.loadOneBike(id).subscribe({
       next: (bike) => {
-        
+
         this.singleBike = bike
+        console.log(bike);
+        console.log(this.currentUser?.user?._id);
+        if (this.singleBike._ownerId._id === this.currentUser?.user?._id) {
+          this.isOwner = true
+        }
+
+
       },
       error: (err) => {
         console.log(err);
