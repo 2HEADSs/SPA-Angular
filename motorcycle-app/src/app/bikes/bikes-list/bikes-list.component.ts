bfbc70bdac79cb75013940ab5d07f2755d3e9195
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-13 14:15:02 +0200

    update css

--- motorcycle-app/src/app/bikes/bikes-list/bikes-list.component.ts
+++ motorcycle-app/src/app/bikes/bikes-list/bikes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 import { IBike } from 'src/app/shared/interfaces/bikes';
 import { BikesService } from '../bikes.service';
@@ -11,21 +12,25 @@ import { BikesService } from '../bikes.service';
 export class BikesListComponent implements OnInit {
 
 
-  bikesList: IBike[] | null = [];
-  hasBikes: boolean = false
+  bikesList: IBike[] | null = null;
+  hasBikes: boolean = true
 
-  constructor(private bikesSerice: BikesService, private authService:AuthService) { }
+  constructor(private bikesSerice: BikesService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.bikesSerice.loadAllBike().subscribe({
       next: (bikes) => {
+        console.log(bikes);
+
         this.bikesList = bikes
         if (this.bikesList.length > 0) {
+
           this.hasBikes = true;
         }
       },
       error: (err) => {
-        console.log(err);
+        this.authService.errorString = err.name
+        this.router.navigate(['/'])
 
       }
     })
