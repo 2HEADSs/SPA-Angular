55c86994b6749da7b017b9a3be9bc6c0d7bcef52
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-13 02:28:32 +0200

    add sesion functionality

---
+++ motorcycle-app/src/app/shared/function/api.ts
@@ -0,0 +1,21 @@
+import { IUser } from './../interfaces/user';
+export const setSession = ({ _id, username, email, accessToken }: IUser) => {
+
+  const user = { _id, username, email, accessToken };
+  sessionStorage.setItem("User", JSON.stringify(user));
+  localStorage.setItem("User", JSON.stringify(user));
+}
+
+export const getSession = () => {
+  const session = sessionStorage.getItem('User')
+  const local = localStorage.getItem('User');
+  return (
+    session ? JSON.parse(session) : null,
+    local ? JSON.parse(local) : null
+  );
+}
+
+export const logoutSession = () => {
+  sessionStorage.removeItem('User');
+  localStorage.removeItem('User');
+}
