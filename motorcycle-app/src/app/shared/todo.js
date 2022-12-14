e233bdffde3a92626cc1ab7274c327006b7798bf
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-14 10:48:48 +0200

    add comments

---
+++ motorcycle-app/src/app/shared/todo.js
@@ -0,0 +1,11 @@
+//todo
+/*
+1. Add guard
+2. Add img on 404
+3. fix errors from server
+4. add email validator in rest
+5. PARSE ERRRORS FOR SERVER
+6. Rendering server errors
+7. Search page
+
+*/
