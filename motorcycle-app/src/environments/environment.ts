7e9b74bc1ce3c08dde322a9deefc297aec5b6d8a
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-17 19:23:28 +0200

    update url

--- motorcycle-app/src/environments/environment.ts
+++ motorcycle-app/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3000'
-  apiUrl: 'https://motorcyleserverspa.vercel.app',
+  apiUrl: 'https://spa-angular-sepia.vercel.app/',
 };
 
 /*
