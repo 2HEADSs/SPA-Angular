2e446e5b6f6042b16240d086bf73916dcffc88b7
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-17 18:29:05 +0200

    add deploy server data

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const session = require('./middlewares/session');
 const router = require('./routes');
 // const connectionString = 'mongodb://127.0.0.1:27017/motorcycle';
 const connectionString = 'mongodb+srv://2HEADS:icIwChZJhMBswg3N@cluster0.69tuqnn.mongodb.net/?retryWrites=true&w=majority';
+// const connectionString = 'https://motorcyleserverspa.vercel.app';
+
 
 
 const initDB = () => mongoose.connect(connectionString)
