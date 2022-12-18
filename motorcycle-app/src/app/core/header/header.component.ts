d076a2c59116c758fc830e3a6baf5fbca84687b7
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-12-18 15:20:00 +0200

    add firebase deploy

--- motorcycle-app/src/app/core/header/header.component.ts
+++ motorcycle-app/src/app/core/header/header.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
-import { getSession } from 'src/app/shared/function/api';
-import { IUser } from 'src/app/shared/interfaces/user';
+
 
 @Component({
   selector: 'app-header',
